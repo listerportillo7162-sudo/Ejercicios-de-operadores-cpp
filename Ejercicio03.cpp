7f25ef3ad3e22297deac5eec8d6948bb7244c1a3
listerportillo7162-sudo <lister.portillo7162@gmail.com>
2026-09-12 20:30:54 -0500

    Create Ejercicio03.cpp

---
+++ Ejercicio03.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int A = 5, B = 10;
+    A += 3; // A = 8
+    B -= A; // B = 2
+    cout << "A=" << A << " B=" << B << endl; // SALIDA: 8, 2
+    return 0;
+}
